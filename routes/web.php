24d33b11ca33936644daff3cd11bd5b2df038f7d
VittorioBarella <vittorio.br@hotmail.com>
2021-06-26 19:20:36 -0300

    Criação do projeto, preparação do ambiente, instalação do composer e desenvolvido da aplicação.

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\SeriesController;
+
+
+
+Route::get('/series', [
+    SeriesController::class, 'index'
+]) ->name('listar_series');
+
+Route::get('/series/criar',[
+    SeriesController::class, 'create'
+]) ->name('form_criar_serie');
+
+Route::post('/series/criar',[
+    SeriesController::class, 'store'
+]);
+
+Route::delete('/series/{id}', [
+    SeriesController::class, 'destroy'
+]);
